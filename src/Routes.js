96f3f80564bc7f8cffe6e049fb8dfc4912ab8f0e
Mare Robert <capnv12@gmail.com>
2019-11-29 08:05:48 +0200

    frontend preparing

--- src/Routes.js
+++ src/Routes.js
@@ -4,16 +4,17 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Signup from './user/Signup'
 import Signin from './user/Signin'
 import Home from './core/Home'
-import Menu from './core/Menu'
+import PrivateRoute from './auth/PrivateRoute'
+import Dashboard from './user/UserDashboard'
 
 const Routes = () => {
     return(
         <BrowserRouter>
-        <Menu />
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/signin" exact component={Signin} />
                 <Route path="/signup" exact component={Signup} />
+                <PrivateRoute path="/user/dashboard" exact component={Dashboard}/>
             </Switch>
         </BrowserRouter>
     )
--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {NavLink, Link, withRouter} from 'react-router-dom'
 import styled from 'styled-components'
 import uuid from 'uuid'
+import {signout, isAuthenticated} from '../auth/index'
 
 
 // const isActive = (history, path) => {
@@ -11,29 +12,51 @@ import uuid from 'uuid'
 //         return { color: "#ffffff" };
 //     }
 // };
-
+// const history = createBrowserHistory()
 const activeClassName = uuid();
 const StyledNavLink = styled(NavLink).attrs({activeClassName})`
   &.${activeClassName} {
     color: red;
   }
 `
-const Menu = () => {
+const Menu = ({history}) => {
     return(
         <div>
-        <ul className="nav nav-tabs bg=primary">
+        <ul className="nav nav-tabs ">
             <li className="nav-item">
                 <StyledNavLink exact className="nav-link" activeClassName="active" to="/">Home</StyledNavLink >
             </li>
             <li className="nav-item">
-                <StyledNavLink  className="nav-link" activeClassName="active" to="/signin">Signin</StyledNavLink >
+                <StyledNavLink exact className="nav-link" activeClassName="active" to="/user/dashboard">Dashboard</StyledNavLink >
             </li>
-            <li className="nav-item">
-                <StyledNavLink  className="nav-link" activeClassName="active" to="/signup">Signup</StyledNavLink >
-            </li>
+            {!isAuthenticated() && (
+                <Fragment>
+                    <li className="nav-item">
+                        <StyledNavLink  className="nav-link" activeClassName="active" to="/signin">Signin</StyledNavLink >
+                    </li>
+                    <li className="nav-item">
+                        <StyledNavLink  className="nav-link" activeClassName="active" to="/signup">Signup</StyledNavLink >
+                    </li>
+                </Fragment>
+            )}
+            {isAuthenticated() && (
+                 <li className="nav-item">
+                    <span
+                     className="nav-link"
+                     style={{ cursor: "pointer", color: "#007bff" }}
+                     onClick={() =>
+                         signout(() => {
+                             history.push("/");
+                         })
+                        }
+                    >
+                     Signout
+                 </span>
+             </li>
+            )}
         </ul>
     </div>
     )
 }
 
-export default Menu
+export default withRouter(Menu)
